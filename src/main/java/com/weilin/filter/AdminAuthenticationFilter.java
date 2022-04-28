e69a00381de5977d1b6666bd6703cc1528cfaa2f
Weilin <2580738656@qq.com>
2022-04-28 16:26:04 +0800

    change

--- src/main/java/com/weilin/filter/AdminAuthenticationFilter.java
+++ src/main/java/com/weilin/filter/AdminAuthenticationFilter.java
@@ -1,40 +1,47 @@
 package com.weilin.filter;
 
+
 import javax.servlet.*;
-import javax.servlet.annotation.WebListener;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.logging.Filter;
-import java.util.logging.LogRecord;
-@WebListener("/admin/*")
+
+@WebFilter("/admin/*")//this filter for all admin url
 public class AdminAuthenticationFilter implements Filter {
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-        throws IOException, ServletException {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-        HttpSession session = httpServletRequest.getSession(false);
-
-        boolean isLoggedIn = (session != null && session.getAttribute("userList") != null);
-        String loginIn = httpServletRequest.getContextPath() + "/admin/login";
-        boolean isLoggedInRequest = httpServletRequest.getRequestURI().equals("login");
-        if (isLoggedIn && (isLoggedInRequest || isLogginpage)) {
-            RequestDispatcher dispatcher = request.getRequestDispatcher("/admin/home");
-            dispatcher.forward(request, response);
-        } else if (isLoggedIn || isLoggedInRequest) {
-            chain.doFilter(request, response);
-        } else {
-            httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/admin/login");
-        }
-
-    }
-    public void destroy(){
-
-    }
-    public void init(FilterConfig fConfig) throws  ServletException{ }
     @Override
-    public boolean isLoggable(LogRecord record) {
-        return false;
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain Chain) throws IOException, ServletException {
+        //change ServletRequest to HttpServletRequest
+        HttpServletRequest httpRequest = (HttpServletRequest)request;
+        HttpServletResponse httpResponse = (HttpServletResponse)response;
+        //get session
+        HttpSession session = httpRequest.getSession(false);
+        boolean isLoggedIn = (session!=null && session.getAttribute("user")!=null);
+        String loginURI = httpRequest.getContextPath()+"/admin/login";
+        boolean isLoginRequest = httpRequest.getRequestURI().equals(loginURI);
+        boolean isLoginPage = httpRequest.getRequestURI().endsWith("login");
+        if (isLoggedIn && (isLoginRequest || isLoginPage)){
+            //the admin is already login ande he is trying login again
+            //then forward to the admin homepage
+            request.getRequestDispatcher("/admin/home").forward(httpRequest,httpResponse);//go to home
+
+        }else if (isLoggedIn || isLoginRequest){
+
+            Chain.doFilter(request,response);//go to next destination
+        }else {
+            System.out.println(httpRequest.getContextPath());
+            httpResponse.sendRedirect(httpRequest.getContextPath()+"/admin/login");//go to login
+        }
+    }
+
+    @Override
+    public void destroy() {
+
     }
 }
--- src/main/java/com/weilin/filter/FrontEndAuthenticationFilter.java
+++ src/main/java/com/weilin/filter/FrontEndAuthenticationFilter.java
@@ -1,61 +1,74 @@
 package com.weilin.filter;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+@WebFilter("/*")//all request
 public class FrontEndAuthenticationFilter implements Filter {
-    private HttpServletRequest httpServletRequest;
-    private static final String[] loginrequiredURLs= {
-            "/updateUser","/logout","/myCart"
-    };
+    private HttpServletRequest httpRequest=null;
+    public static final String[] loginRequiredURLs={"/updateUser","/logout","/cart",
+            "/accountDetails","/order"};//url which need login
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        throw IOException,ServletException{
-            httpServletRequest=(HttpServletRequest) servletRequest;
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain Chain) throws IOException, ServletException {
 
-            String path =httpServletRequest.getRequestURI().substring(httpServletRequest.getContextPath().length());
-
-            if(path.startsWith("/admin")){
-                filterChain.doFilter(servletRequest,servletResponse);
-                return;
-            }
-            HttpSession session=httpServletRequest.getSession(false);
-            boolean isLoggedIn =(session !=null&&session.getAttribute("userList")!=null);
-            String loginURI=httpServletRequest.getContextPath() +"/login";
-            boolean isLoginRequest =httpServletRequest.getRequestURI().equals(loginURI);
-            boolean isLoginPage =httpServletRequest.getRequestURI().endsWith("login");
-
-            if(isLoggedIn && (isLoginRequest || isLoginPage)) {
-                httpServletRequest.getRequestDispatcher("/").forward(servletRequest,servletResponse);
-            }else if(!isLoggedIn && isLoginRequird()){
-                String loginPage = "/login";
-                RequestDispatcher dispatcher =httpServletRequest.getRequestDispatcher(loginPage);
-                dispatcher.forward(servletRequest.servletResponse);
-            }else {
-                filterChain.doFilter(servletRequest,servletResponse);
-            }
+        httpRequest = (HttpServletRequest)request;//change ServletRequest to HttpServletRequest
+        //get path
+        String path = httpRequest.getRequestURI().substring(httpRequest.getContextPath().length());
+        if (path.startsWith("/admin/")){
+            Chain.doFilter(request,response);//go to next filter -AdminAuthenticationFilter
+            return;
         }
-    }
-    private boolean isLoginRequird(){
-        String requestURL = httpServletRequest.getRequestURI().toString();
 
-        for (String loginRequiredURL : loginrequiredURLs){
-            if (requestURL.contains(loginRequiredURL)){
-                return  true;
+        //get session
+        HttpSession session = httpRequest.getSession(false);
+        boolean isLoggedIn = (session!=null && session.getAttribute("user")!=null);
+        String loginURI = httpRequest.getContextPath()+"/login";//for public user  - not admin
+        boolean isLoginRequest = httpRequest.getRequestURI().equals(loginURI);
+        boolean isLoginPage = httpRequest.getRequestURI().endsWith("login");
+
+        if (isLoggedIn && (isLoginRequest || isLoginPage)){
+            //the admin is already login ande he is trying login again
+            //then forward to the public user homepage
+            //update 2 ServletRequest and ServletResponse
+            httpRequest.getRequestDispatcher("/").forward(request,response);//go to home -public user
+            //update 3 i miss !-not
+        }else if (!isLoggedIn && isLoginRequired()){//check in loginRequiredURLs array
+            //if your is not login in and the requested page requires login
+            //then forward to the login page
+            //update- 4- use
+            request.getRequestDispatcher("/login").forward(request,response);
+        }else {
+            //for other page which not requires login
+            Chain.doFilter(request,response);//go to page
+        }
+
+    }
+    /*
+    method for check requested url need login
+     */
+    private boolean isLoginRequired() {
+        //update 1 url not uri
+        String requestURI = httpRequest.getRequestURI().toString();//get url
+
+        for (String loginRequiredURL : loginRequiredURLs){//get one by one from array
+            //check
+            if (requestURI.contains(loginRequiredURL)){
+                return true;
             }
         }
         return false;
     }
+
     @Override
     public void destroy() {
 
     }
-
 }
