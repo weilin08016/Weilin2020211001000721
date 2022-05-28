3e22d6e2e5f1895517b9203679358cf8e1d1ec26
Weilin <2580738656@qq.com>
2022-05-28 18:06:41 +0800

    Week10

---
+++ src/main/java/com/weilin/week10/StringBean.java
@@ -0,0 +1,14 @@
+package com.weilin.week10;
+
+public class StringBean {
+    // properties
+    private String message;
+    // get method
+    public String getMessage() {
+        return message;
+    }
+    // set method
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
